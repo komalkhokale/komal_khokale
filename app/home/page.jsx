ede92ec739e2f0033c78a3d2038fe6db3af34fb5
komalkhokale <kkhokale15@gmail.com>
2025-12-15 08:02:46 +0530

    add resume

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -69,7 +69,12 @@ const HeroContent = () => {
           whileHover={{ scale: 1.08 }}
           whileTap={{ scale: 0.95 }}
         >
-          Download Resume
+           <a
+            href="/komal_khokale.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="resume-btn"
+          >Download Resume</a>
         </motion.button>
       </motion.div>
 
